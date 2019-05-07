959fcc7d8ce4fb6bc4367b362002e18476f811a3
LEduard0 <leduardo152@gmail.com>
2019-05-07 15:58:19 -0300

    teste

--- js/testes.js
+++ js/testes.js
@@ -5,3 +5,16 @@ function validateForm() {
         return false;
     }
 }
+
+function veificaNumero() {
+    var x, text;
+
+    x = document.getElementById("numero").value;
+
+    if (isNaN(x) || x < 1 || x > 10) {
+        text = "Input not valid";
+    } else {
+        text = "Input OK";
+    }
+    document.getElementById("msgErro").innerHTML = text;
+}
