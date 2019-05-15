d459c69c1d45adfe8586375a81a5a898e9c803de
LEduard0 <leduardo152@gmail.com>
2019-05-15 15:51:08 -0300

    gulp com minify js

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,18 @@
-var gulp = require('gulp');
+const gulp = require('gulp');
 
 gulp.task('default', function a(done) {
     done();
 });
+
+const { src, dest } = require('gulp');
+const uglify = require('gulp-uglify');
+const rename = require('gulp-rename');
+
+exports.js = function() {
+    return src('src/js/*.js')
+        // The gulp-uglify plugin won't update the filename
+        .pipe(uglify())
+        // So use gulp-rename to change the extension
+        .pipe(rename({ extname: '.min.js' }))
+        .pipe(dest('js/'));
+}
